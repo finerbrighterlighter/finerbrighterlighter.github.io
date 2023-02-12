a0c3df549582f6d9bcd3f522488a2426b6fbfa09
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2023-02-12 15:07:45 +0700

    Update lastFM.js

--- assets/js/lastFM.js
+++ assets/js/lastFM.js
@@ -42,7 +42,7 @@ function lastFM_request(method, username, API_key, number, elementID) {
 					for (i = 0; i < number; i++) {
 						var album = obj.topalbums.album[i]; // references this specific artist
 						// console.log(artist);
-						element.innerHTML += "<a href='" + album.url + "' target='_blank' rel='noreferrer noopener'><img src='" + album.image[1]["#text"] + "' alt='" + album.name + "' style="filter: grayscale(50%);"/><a>";
+						element.innerHTML += "<a href='" + album.url + "' target='_blank' rel='noreferrer noopener'><img src='" + album.image[1]["#text"] + "' alt='" + album.name + "' style='filter: grayscale(50%)';/><a>'";
 					}
 				}
 			 }
