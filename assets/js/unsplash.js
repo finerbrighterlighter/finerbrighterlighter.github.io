b63f327c0289dc2c5fdd36036eb5f3cdd55deac5
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2023-02-17 10:29:58 +0700

    unsplash random to latest

--- assets/js/unsplash.js
+++ assets/js/unsplash.js
@@ -1,8 +1,23 @@
-function random_unsplash(username, size, number, elementID){
-	var element = document.getElementById("img-container");
-	for (i = 0; i < number; i++) {
-		element.innerHTML += '<img src="https://source.unsplash.com/user/'+username+'/'+size+'x'+size+'?sig='+i+'">'
-	}
+function getunsplash(username, accessKey, number, elementID){
+	var element   = document.getElementById(elementID);
+	fetch(`https://api.unsplash.com/users/${username}/photos?per_page=10`, {
+  	headers: {
+  		Authorization: `Client-ID ${accessKey}`,
+  	},
+	})
+  .then((response) => response.json())
+  .then((data) => {
+  	const time = data.map((photo) => photo.created_at);
+  	const unsplashUrls = data.map((photo) => photo.links.html);
+    const photoUrls = data.map((photo) => photo.urls.thumb);
+    for (i = 0; i < number; i++) {
+    	element.innerHTML += "<a href='" + unsplashUrls[i] + "' target='_blank' rel='noreferrer noopener'><img src='" + photoUrls[i] + "' title='" + time[i].split(/T/)[0] + "' alt='" +  time[i].split(/T/)[0]  + "'style='filter: grayscale(50%);width:64px;height:128px;object-fit:cover';/>";
+    }
+  })
+  .catch((error) => {
+  	console.error("Error fetching data from Unsplash API:", error);
+  })
 }
 
-setInterval(random_unsplash('finerbrighterlighter', '64', '10', 'img-container'), 5000);
+setInterval(getunsplash("finerbrighterlighter", "bQGcIoNwvjNVym1Dr-50ReObTxnIU_D4_oMy6XgRIaM", 20, "latestimage"), 5000);
+
