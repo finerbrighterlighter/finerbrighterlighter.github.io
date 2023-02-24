fde2c46824755e6cb5ed58c134f90920729d094e
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2023-02-24 10:56:21 +0700

    scroll push

--- assets/js/randomvalue.js
+++ assets/js/randomvalue.js
@@ -13,14 +13,14 @@ function updateElement_interval(elementId, valuesArray, interval, type) {
   }, interval);
 }
 
-function updateElement_animation(elementId, valuesArray, animation, event) {
+function updateElement_animation(elementId, containerId, valuesArray, animation, event) {
   const element = document.getElementById(elementId);
+  const box = document.getElementById(containerId);
   
-  if (event.animationName === animation) {
-    const randomIndex = Math.floor(Math.random() * valuesArray.length);
-    const randomValue = textArray[randomIndex];
-    element.innerHTML = randomValue;
-  }
+  const randomIndex = Math.floor(Math.random() * valuesArray.length);
+  const randomValue = textArray[randomIndex];
+  element.innerHTML = randomValue;
+  box.style.animation = "scroll-text 5s linear infinite, color-animation 1s linear infinite";	  
 }
 
 const textValues = ["Let's donate blood","It's in you to give","Red with life","Life is blood; Blood is life","သွေးသည်အသက်​ အသက်သည်သွေး","Every blood donor is a hero","Give the gift of life","Safe blood for saving mothers","Thank you for saving my life","Blood connects us all","Give Blood. Give Now. Give Often","Be there for someone else","Give blood. Share life","Safe Blood For All","Safe Blood Saves Lives","Give blood and keep the world beating","Blood donation is an act of solidarity", "Join the effort and save lives","Safe Life Give Blood"];
@@ -30,4 +30,4 @@ const imageValues = ["profile (1).jfif", "profile (1).jpg", "profile (2).jpg", "
 updateElement_interval("profile", imageValues, 20000, "image");
 
 const textElement = document.getElementById("banner-group");
-textElement.addEventListener("animationend", function(event) {updateElement_animation("banner-group", textValues, "scroll-text", event);});
+textElement.addEventListener("animationend", function(event) {updateElement_animation("banner", "banner-group", textValues, "scroll-text", event);});
