332e8fbab9e413b7ec9a15115406cdee5971b868
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2023-02-24 10:16:51 +0700

    random value on animation

--- assets/js/randomvalue.js
+++ assets/js/randomvalue.js
@@ -1,4 +1,4 @@
-function updateElementWithRandomValues(elementId, valuesArray, interval, type) {
+function updateElement_interval(elementId, valuesArray, interval, type) {
   const element = document.getElementById(elementId);
 
   setInterval(() => {
@@ -13,12 +13,21 @@ function updateElementWithRandomValues(elementId, valuesArray, interval, type) {
   }, interval);
 }
 
-const textValues = ["Let's donate blood","It's in you to give","Red with life","Life is blood; Blood is life","သွေးသည်အသက်​ အသက်သည်သွေး","Every blood donor is a hero","Give the gift of life : donate blood","Safe blood for saving mothers","Thank you for saving my life","Blood connects us all","Give Blood. Give Now. Give Often","Be there for someone else. Give blood. Share life","Safe Blood For All","Safe Blood Saves Lives","Give blood and keep the world beating","Blood donation is an act of solidarity. Join the effort and save lives","Safe Life Give Blood"];
+function updateElement_animation(elementId, valuesArray, animation, event) {
+  const element = document.getElementById(elementId);
+  
+  if (event.animationName === animation) {
+    const randomIndex = Math.floor(Math.random() * valuesArray.length);
+    const randomValue = textArray[randomIndex];
+    element.innerHTML = randomValue;
+  }
+}
+
+const textValues = ["Let's donate blood","It's in you to give","Red with life","Life is blood; Blood is life","သွေးသည်အသက်​ အသက်သည်သွေး","Every blood donor is a hero","Give the gift of life","Safe blood for saving mothers","Thank you for saving my life","Blood connects us all","Give Blood. Give Now. Give Often","Be there for someone else","Give blood. Share life","Safe Blood For All","Safe Blood Saves Lives","Give blood and keep the world beating","Blood donation is an act of solidarity", "Join the effort and save lives","Safe Life Give Blood"];
 
 const imageValues = ["profile (1).jfif", "profile (1).jpg", "profile (2).jpg", "profile (3).jpg", "profile (4).jpg", "profile (5).jpg", "profile (6).jpg", "profile (7).jpg", "profile (8).jpg", "profile (9).jpg", "profile (10).jpg"];
 
-const textElementId = "myText";
-const imageElementId = "myImage";
+updateElement_interval("profile", imageValues, 20000, "image");
 
-updateElementWithRandomValues("banner", textValues, 4000, "text");
-updateElementWithRandomValues("profile", imageValues, 20000, "image");
+const textElement = document.getElementById("animate-charcter");
+textElement.addEventListener("animationend", function(event) {updateElement_animation("banner", textValues, "scroll-text", event);});
