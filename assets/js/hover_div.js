c267cb7425b3f2aa558f914dc750b3a4697343d5
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2023-05-18 10:39:37 +0700

    nothing much

--- assets/js/hover_div.js
+++ assets/js/hover_div.js
@@ -15,6 +15,6 @@ function showHiddenDivOnHover(hoverTargetSelector, hiddenDivId, htmlBlock) {
     });
   }
 
-var htmlblock = '<span id="hideblock"><i class="fa-li fa fas fa-newspaper" style="position:initial"></i>Academic Publication <i class="fa-li fa fas fa-chalkboard" style="position:initial"></i>Conference Proceeding/Poster <i class="fa-li fa fas fa-microphone-alt" style="position:initial"></i>Conference Speaking <i class="fa-li fa fas fa-book" style="position:initial"></i>Dissertation/Descriptive Report </span>';
+var htmlblock = '<span id="hideblock" style="font-size:80%;font-family: monospace, monospace;color:black;border:1px dotted grey;padding:0;background:#e7c336;"><i class="fa-li fa fas fa-newspaper" style="position:initial"></i>Academic Publication <i class="fa-li fa fas fa-chalkboard" style="position:initial"></i>Conference Proceeding/Poster <i class="fa-li fa fas fa-microphone-alt" style="position:initial"></i>Conference Speaking <i class="fa-li fa fas fa-book" style="position:initial"></i>Dissertation/Descriptive Report </span>';
 
 showHiddenDivOnHover('.work_div', 'legend-div', htmlblock);
