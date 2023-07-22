080af632ed8a717be0df6abe77ac867c4c52df8a
fibrili <55363976+finerbrighterlighter@users.noreply.github.com>
2023-07-22 15:00:23 +0700

    timezone push

--- assets/js/currenttime.js
+++ assets/js/currenttime.js
@@ -1,14 +1,22 @@
-function getTime(elementID, timezone, format) {
+function getTime(elementID, timezone, format, value) {
   element = document.getElementById(elementID);
   const date = new Date();
   const options = {
     timeZone: timezone,
+    timeZoneName: "long",
     hour12: format,
     hour: "numeric",
     minute: "numeric",
   };
   const formattedTime = date.toLocaleString("en-US", options);
-  element.innerText = formattedTime;
+  if (value=="CurTime"){
+  	var result = formattedTime.split("M")[0] + "M";
+  } else if (value=="timezone"){
+  	var result = formattedTime.split("M")[1];
+  }
+  element.innerText = result;
 }
 
-setInterval(getTime("current_time", "Asia/Bangkok", true), 1000); // Refresh time every second
+setInterval(getTime("timezone", "Asia/Bangkok", true, "timezone"), 1000);
+setInterval(getTime("current_time", "Asia/Bangkok", true, "CurTime"), 1000); // Refresh time 
+
